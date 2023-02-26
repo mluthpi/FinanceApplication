958b31f6686ad71b8a6b284c95c1bbd6fbe709af
muhammad Luthfiyanto <luthfiyantomuhammad@gmail.com>
2023-02-26 11:23:23 +0700

    second commit

--- app/src/main/java/com/example/financeapplication/LoginActivity.kt
+++ app/src/main/java/com/example/financeapplication/LoginActivity.kt
@@ -14,17 +14,22 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-
+        val email = "admin"
+        val password = "admin"
 
         binding.btnLogin.setOnClickListener {
-            if (binding.edtEmail.text.toString().equals("admin") && binding.edtPassword.text.toString().equals("admin")) {
+            if (binding.edtEmail.text.toString()
+                    .equals(email) && binding.edtPassword.text.toString().equals(password)
+            ) {
                 Intent(this@LoginActivity, MainActivity::class.java).also {
                     startActivity(it)
                 }
-                Toast.makeText(this,"LOGIN SUCCESS", Toast.LENGTH_SHORT).show()
-            } else  {
-                Toast.makeText(this,"LOGIN FAILED", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show()
             }
         }
     }
-}
+
+
+
+    }
